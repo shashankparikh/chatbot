97f0d7709842204b53bc8a095bf241bc913b9d8d
Shashank Parikh <shashank@moonraft.com>
2020-05-12 10:08:11 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -10,20 +10,20 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
-const uri = process.env.DB_URI
-mongoose
-  .connect(
-    uri,
-    // 'mongodb://localhost:27017/InnerWheel',
-    { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
-  )
-  .then(() => {
-    console.log(`connection to database established`)
-  })
-  .catch(err => {
-    console.log(`db error ${err}`)
-    process.exit(-1)
-  })
+// const uri = process.env.DB_URI
+// mongoose
+//   .connect(
+//     uri,
+//     // 'mongodb://localhost:27017/InnerWheel',
+//     { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true }
+//   )
+//   .then(() => {
+//     console.log(`connection to database established`)
+//   })
+//   .catch(err => {
+//     console.log(`db error ${err}`)
+//     process.exit(-1)
+//   })
 
 const getDataRouter = require('./routes/getData')
 const postDataRouter = require('./routes/postData')
